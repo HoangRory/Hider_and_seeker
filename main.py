ecbfd22bc2cdadd0dbfee85ea1148fbc4d1931dc
Muramasa94 <22127057@student.hcmus.edu.vn>
2024-04-06 12:14:08 +0700

    Improved menu visualization

--- main.py
+++ main.py
@@ -3,13 +3,36 @@ import sys
 from button import Button
 import os
 
-HEITGH = 600
-WIDTH = 800
+HEITGH = 720
+WIDTH = 1280
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEITGH))
-menuBG = pygame.image.load("assets/MenuBG.png")
+menuBG = pygame.image.load("assets/MenuBG.jpg")
+buttonBG = pygame.image.load("assets/Button.png")
+buttonBG = pygame.transform.scale(buttonBG, (250, 100))
+
+def printText(inputText, size, color, pos):
+    font = pygame.font.Font(None, size)
+    text = font.render(inputText, True, color)
+    textRect = text.get_rect(center=pos)
+    textOutlineUp = font.render(inputText, True, (0, 0, 0))
+    textRectOutlineUp = textOutlineUp.get_rect(center=(pos[0], pos[1] - 2))  # Slightly offset for outline
+    screen.blit(textOutlineUp, textRectOutlineUp)
+
+    textOutlineDown = font.render(inputText, True, (0, 0, 0))
+    textRectOutlineDown = textOutlineDown.get_rect(center=(pos[0], pos[1] + 2))  # Slightly offset for outline
+    screen.blit(textOutlineDown, textRectOutlineDown)
+
+    textOutlineLeft = font.render(inputText, True, (0, 0, 0))
+    textRectOutlineLeft = textOutlineLeft.get_rect(center=(pos[0] - 2, pos[1]))  # Slightly offset for outline
+    screen.blit(textOutlineLeft, textRectOutlineLeft)
+
+    textOutlineRight = font.render(inputText, True, (0, 0, 0))
+    textRectOutlineRight = textOutlineRight.get_rect(center=(pos[0] + 2, pos[1]))  # Slightly offset for outline
+    screen.blit(textOutlineRight, textRectOutlineRight)
+    screen.blit(text, textRect)
 
 def menu():
     pygame.display.set_caption("Hide and seek")
@@ -17,15 +40,13 @@ def menu():
     while True:
         screen.blit(menuBG, (0, 0))
         mousePos = pygame.mouse.get_pos()
-        menuText = pygame.font.Font(None, 100).render("HIDE AND SEEK", True, (255, 255, 255))
-        menuRect = menuText.get_rect(center=(WIDTH/2, HEITGH/2 - 200))
-        subtitleText = pygame.font.Font(None, 35).render("(But you're not playing)", True, (255, 255, 255))
-        subtitleRect = subtitleText.get_rect(center=(WIDTH/2, HEITGH/2 - 125))
+
+        menuText = "HIDE AND SEEK"
+        printText(menuText, 150, (255, 255, 255), (WIDTH/2, HEITGH/2 - 200))
+        subtitleText = "But you're not playing"
+        printText(subtitleText, 50, (255, 255, 255), (WIDTH/2, HEITGH/2 - 125))
         
-        startButton = Button(image = None, pos = (WIDTH/2, HEITGH/2), text = "Start", font = pygame.font.Font(None, 75), textColor = "White", hoverColor = "Green")
-        
-        screen.blit(menuText, menuRect)
-        screen.blit(subtitleText, subtitleRect)
+        startButton = Button(image=buttonBG, pos=(WIDTH/2, HEITGH/2 + 50), text="START", font=pygame.font.Font(None, 75), textColor="White", hoverColor="Green")
         
         for button in [startButton]:
             button.changeColor(mousePos)
@@ -40,19 +61,19 @@ def menu():
                     start()
                     
         pygame.display.update()
-        
+
+lvButton = pygame.image.load("assets/Button.png")
+lvButton = pygame.transform.scale(lvButton, (175, 80))
 def start():
     while True:
         screen.blit(menuBG, (0, 0))
         mousePos = pygame.mouse.get_pos()
-        promptText = pygame.font.Font(None, 75).render("Choose a level", True, (255, 255, 255))
-        promptRect = promptText.get_rect(center=(WIDTH/2, HEITGH/2 - 200))
+        promptText = "Choose a level"
+        printText(promptText, 100, (255, 255, 255), (WIDTH/2, HEITGH/2 - 200))
         
-        screen.blit(promptText, promptRect)
-        
-        level1Button = Button(image = None, pos = (WIDTH/2, HEITGH/2), text = "Level 1", font = pygame.font.Font(None, 50), textColor = "White", hoverColor = "Green")
-        level2Button = Button(image = None, pos = (WIDTH/2, HEITGH/2 + 50), text = "Level 2", font = pygame.font.Font(None, 50), textColor = "White", hoverColor = "Green")
-        level3Button = Button(image = None, pos = (WIDTH/2, HEITGH/2 + 100), text = "Level 3", font = pygame.font.Font(None, 50), textColor = "White", hoverColor = "Green")
+        level1Button = Button(image=lvButton, pos=(WIDTH/2, HEITGH/2 - 50), text="Level 1", font=pygame.font.Font(None, 50), textColor="White", hoverColor="Green")
+        level2Button = Button(image=lvButton, pos=(WIDTH/2, HEITGH/2 + 50), text="Level 2", font=pygame.font.Font(None, 50), textColor="White", hoverColor="Green")
+        level3Button = Button(image=lvButton, pos=(WIDTH/2, HEITGH/2 + 150), text="Level 3", font=pygame.font.Font(None, 50), textColor="White", hoverColor="Green")
         
         for button in [level1Button, level2Button, level3Button]:
             button.changeColor(mousePos)
